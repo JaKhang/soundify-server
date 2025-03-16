b47a15358d92a4e850fb9e2e68181c5d80de848e
JaKhang <kamilionbc@gmail.com>
2025-03-16 17:07:06 +0700

    add base project

---
+++ src/main/java/com/soundify/server/SoundifyServerApplication.java
@@ -0,0 +1,13 @@
+package com.soundify.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundifyServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoundifyServerApplication.class, args);
+    }
+
+}
