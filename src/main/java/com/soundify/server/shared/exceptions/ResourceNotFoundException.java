6e82f4f91aad64fa70b7c48a39ca198477d0c9db
dwnqprogramming <dwnq.coding@gmail.com>
2025-03-28 15:29:05 +0700

    Add @SuperBuilder and create public constructor for ResourceNotFoundException

--- src/main/java/com/soundify/server/shared/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/soundify/server/shared/exceptions/ResourceNotFoundException.java
@@ -1,6 +1,10 @@
 package com.soundify.server.shared.exceptions;
 
 public class ResourceNotFoundException extends SystemException {
+    public ResourceNotFoundException(String message) {
+        super(message, ErrorCode.NOT_FOUND);
+    }
+
     protected ResourceNotFoundException() {
         super(ErrorCode.NOT_FOUND);
     }
