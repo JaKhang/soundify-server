cb9c355c9a0e37186407c58ca1f1f06fef6b9739
dwnqprogramming <dwnq.coding@gmail.com>
2025-03-28 16:00:00 +0700

    Delete BadRequestException

--- src/main/java/com/soundify/server/shared/exceptions/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.soundify.server.shared.exceptions;
-
-public class BadRequestException extends SystemException {
-    public BadRequestException(String message) {
-        super(message, ErrorCode.INVALID_REQUEST);
-    }
-}
