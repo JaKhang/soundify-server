edac89a353ab6eeb5173a88f68466c187ab207bf
JaKhang <kamilionbc@gmail.com>
2025-04-14 16:08:48 +0700

    update security

--- src/main/java/com/soundify/server/shared/security/Principal.java
+++ src/main/java/com/soundify/server/shared/security/Principal.java
@@ -3,6 +3,7 @@ package com.soundify.server.shared.security;
 import com.soundify.server.shared.domain.Id;
 import org.springframework.security.core.GrantedAuthority;
 
+import java.time.LocalDate;
 import java.util.Collection;
 import java.util.Locale;
 
@@ -10,4 +11,5 @@ public interface Principal {
     Id id();
     Collection<? extends GrantedAuthority> authorities();
     Locale locale();
+    LocalDate dateOfBirth();
 }
