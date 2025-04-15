38924e8feaf3666d8cf832fd4201415cc51cef69
JaKhang <kamilionbc@gmail.com>
2025-04-15 21:33:24 +0700

    update base

--- src/test/java/com/soundify/server/SoundifyServerApplicationTests.java
+++ src/test/java/com/soundify/server/SoundifyServerApplicationTests.java
@@ -3,11 +3,7 @@ package com.soundify.server;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class SoundifyServerApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
 }
